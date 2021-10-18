a6ad6d844dbf5c61f4444c46d51fbc7bd65d2cad
sasmita <sasmitapradhan01@gmail.com>
2021-10-17 22:27:50 -0400

    Ok commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Updated Hi Jenkins Hello World");
+		System.out.println("OK Updated Hi Jenkins Hello World");
 	}
 }
